60006ce92368fb3ddbf68fba2c1d522acfcb9b43
Jakub <2474411+vetvicka@users.noreply.github.com>
2025-12-31 19:43:28 +0100

    remove dummy tests

--- 01/solver.test.ts
+++ 01/solver.test.ts
@@ -1,16 +1,7 @@
-import { solve } from './solver';
-import { parse } from './parser';
+import { solve1 } from './solver';
 
 describe('01 solver', () => {
   test('empty input', () => {
-    expect(solve(parse(''))).toBe(0);
-  });
-
-  test('single zero occurrence', () => {
-    expect(solve(parse('R50'))).toBe(1);
-  });
-
-  test('multiple zeros', () => {
-    expect(solve(parse('R50\nL100'))).toBe(2);
+    expect(solve1([])).toBe(0);
   });
 });
